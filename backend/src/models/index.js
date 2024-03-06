cff95f7a7c65285d90d9de602c120e0cfd61f924
Aridane1 <aridanejdr6@gmail.com>
2024-03-06 11:43:47 +0000

    implement the auth

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 import { sequelize } from "../config/sequelizeConfig.js";
-import createUsertModel from "./user.model.js";
+import createUserModel from "./user.model.js";
 
 export const db = {};
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.User = createUsertModel(sequelize, Sequelize);
+db.User = createUserModel(sequelize, Sequelize);
