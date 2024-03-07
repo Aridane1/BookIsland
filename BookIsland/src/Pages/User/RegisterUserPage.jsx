b59aa3ed7ef7429d82e130b3ecfb7157bea82c39
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-03-07 12:14:21 +0100

    #8

--- BookIsland/src/Pages/User/RegisterUserPage.jsx
+++ BookIsland/src/Pages/User/RegisterUserPage.jsx
@@ -1,6 +1,13 @@
+import { RegisterForm } from "../../Components/Partials/Forms/Register/RegisterForm"
+import { SimpleFooter } from "../../Components/Partials/SimpleFooter"
+import { SimpleHeader } from "../../Components/Partials/SimpleHeader"
 
 export const RegisterUserPage = () => {
   return (
-    <div>RegisterUserPage</div>
-  )
+    <>
+<SimpleHeader></SimpleHeader>
+<RegisterForm></RegisterForm>
+<SimpleFooter></SimpleFooter>
+    </>
+    )
 }
