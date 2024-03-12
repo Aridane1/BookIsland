5a333cea14669ae281984c3d985ed31c4cbfbff4
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-03-12 13:47:37 +0100

    #35 animation on home fixed

--- BookIsland/src/Components/Feed/EyeCatcher.jsx
+++ BookIsland/src/Components/Feed/EyeCatcher.jsx
@@ -6,11 +6,12 @@ export const EyeCatcher = () => {
         <img src={EyeCatcherImage} alt="" className="relative w-full" />
         <div className="">
 
-        <span className="absolute text-light text-[32px] font-bold top-[230px] left-[240px]">Read</span>
+        <span className="absolute text-light text-[32px] font-bold top-[230px] left-[240px] ">Read</span>
         <span className="absolute text-light text-[32px] font-bold top-[260px] left-[200px]">Something</span>
-        <span id="changeText" className="absolute top-[290px] left-[250px]">New</span>
+        <span id="changeText" className="absolute top-[290px] left-[220px] w-[100px] text-center">New</span>
         <span className="absolute block w-fit top-[320px] left-[180px] text-[32px] font-bold text-light">Every week!</span>
         </div>
+    
     </div>
   )
 }
