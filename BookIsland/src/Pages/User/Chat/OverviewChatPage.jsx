9ad136122e3787528c66b4029d84ff373422ffa2
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-03-14 11:02:44 +0100

    #71 about and Overview chat fixed

--- BookIsland/src/Pages/User/Chat/OverviewChatPage.jsx
+++ BookIsland/src/Pages/User/Chat/OverviewChatPage.jsx
@@ -50,48 +50,49 @@ useEffect(() => {
   return (
     <>
       <Header />
-      <div className=" flex justify-center items-center w-screen py-4">
-      <img 
-        src={TestProfilImag} 
-        alt="profile image"
-        className="w-20 h-20 aspect-square object-cover rounded-full flex items-center justify-center"
-      />
-      </div>
+      <div className="py-20">
+        <div className=" flex justify-center items-center w-screen py-4">
+        <img 
+          src={TestProfilImag} 
+          alt="profile image"
+          className="w-20 h-20 aspect-square object-cover rounded-full flex items-center justify-center"
+        />
+        </div>
 
-      <div className="px-[18px] py-4 ">
-        <p className="text-dark font-bold text-[32px] w-fit pt-2 ">Your chat</p>
-        <hr className="relative -top-[30px] w-[84px] h-1 my-8 bg-dark border-0 rounded "></hr>
-      </div>
-      {shouldShowElement === "deleting" && (
-        <div className="-mt-8">
+        <div className="px-[18px] py-4 ">
+          <p className="text-dark font-bold text-[32px] w-fit pt-2 ">Your chat</p>
+          <hr className="relative -top-[30px] w-[84px] h-1 my-8 bg-dark border-0 rounded "></hr>
+        </div>
+        {shouldShowElement === "deleting" && (
+          <div className="-mt-8">
 
-          <div className="absolute w-[68px] mx-[18px] h-20 bg-[#FF6565] flex justify-center items-center rounded-lg">
-            <FaCirclePlus className="text-light text-[40px] rotate-45" />
+            <div className="absolute w-[68px] mx-[18px] h-20 bg-[#FF6565] flex justify-center items-center rounded-lg">
+              <FaCirclePlus className="text-light text-[40px] rotate-45" />
+            </div>
           </div>
-        </div>
-       )}
-      {!(shouldShowElement === "deleted") && (
-        
-      <div 
-        className="grid grid-cols-6 px-[18px] -mt-[30px] "
-        onTouchStart={onTouchStart} 
-        onTouchMove={onTouchMove} 
-        onTouchEnd={onTouchEnd}
-        id="chatElement"
-      >
-        <div className=" col-span-1">
-          <img 
-            src={TestProfilImag} 
-            alt="image of chat profile"
-            className=" w-16 h-16 aspect-square object-cover rounded-full flex items-center justify-center"
-          />
-        </div>
-        <div className=" pl-4 col-span-5">
-          <h3 className="text-primary font-bold text-[22px] ">Test testersen</h3> {/*username of chat*/}
-          <p className="text-dark">Lorem, ipsum dolor sit amet consectetur adipisicing elit..{" "} </p>
-        </div>
-      </div> 
-    )}
+        )}
+        {!(shouldShowElement === "deleted") && (  
+          <div 
+            className="grid grid-cols-6 px-[18px] -mt-[30px]  "
+            onTouchStart={onTouchStart} 
+            onTouchMove={onTouchMove} 
+            onTouchEnd={onTouchEnd}
+            id="chatElement"
+          >
+            <div className=" col-span-1 ">
+              <img 
+                src={TestProfilImag} 
+                alt="image of chat profile"
+                className=" w-16 h-16 aspect-square object-cover rounded-full flex items-center justify-center"
+              />
+            </div>
+            <div className=" pl-4 col-span-5">
+              <h3 className="text-primary font-bold text-[22px] ">Test testersen</h3> {/*username of chat*/}
+              <p className="text-dark">Lorem, ipsum dolor sit amet consectetur adipisicing elit..{" "} </p>
+            </div>
+          </div> 
+        )}
+    </div>
       <Footer />
     </>
   )
